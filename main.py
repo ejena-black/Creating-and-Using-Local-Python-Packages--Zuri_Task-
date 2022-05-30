ab2b2f8b3971607f15575ce31adbab3fe1666a33
Nathan Onofeghara <nathanonofeghara@gmail.com>
2022-05-30 23:19:32 +0100

    refactored player logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 game_options = ['R', 'P', 'S']
 
 
-# Computer logic
+# Computer_logic
 def computer_move():
     computer_choice = random.choice(game_options)
     if computer_choice == 'R':
@@ -16,25 +16,28 @@ def computer_move():
     return computer_choice
 
 
+# Player_logic
+while True:
+    player_choice = input('Pick a move "R", "P", "S":  ')
+    player_choice = player_choice.upper()
+
+    if player_choice in game_options:
+        if player_choice == 'R':
+            player_choice= 'Rock'
+        elif player_choice == 'P':
+            player_choice = 'Paper'
+        elif player_choice == 'S':
+            player_choice = 'Scissors'
+        break
+        
+    else:
+        print('Invalid input, Please pick from the options given \n')
+    
 
 
-# Player logic
-def player_move():
 
-    while True:
-        player_choice = input('Pick a move "R", "P", "S":  ')
-        player_choice = player_choice.upper()
 
-        if player_choice in game_options:
-            if player_choice == 'R':
-                player_choice= 'Rock'
-            elif player_choice == 'P':
-                player_choice = 'Paper'
-            elif player_choice == 'S':
-                player_choice = 'Scissors'
-
-            return player_choice
-        else:
-            print('Invalid input, Please pick from the options given \n')
-
+# Game prompt
+print('Welcome to the Rock, Paper, Scissors game  \nThis game is Player vs CPU')
+print('"R" is for "Rock"  \n"P" is for "Paper" \n"S" is for "Scissors" ')
 
