40a42647f2e5c0ad1d4c62b1a362b0af72b2d888
Nathan Onofeghara <nathanonofeghara@gmail.com>
2022-05-30 22:53:17 +0100

    completed player logic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import random
 
 game_options = ['R', 'P', 'S']
 
+
+# Computer logic
 def computer_move():
     computer_choice = random.choice(game_options)
     if computer_choice == 'R':
@@ -13,5 +15,26 @@ def computer_move():
 
     return computer_choice
 
+
+
+
+# Player logic
+def player_move():
+
+    while True:
+        player_choice = input('Pick a move "R", "P", "S":  ')
+        player_choice = player_choice.upper()
+
+        if player_choice in game_options:
+            if player_choice == 'R':
+                player_choice= 'Rock'
+            elif player_choice == 'P':
+                player_choice = 'Paper'
+            elif player_choice == 'S':
+                player_choice = 'Scissors'
+
+            return player_choice
+        else:
+            print('Invalid input, Please pick from the options given \n')
 
 
