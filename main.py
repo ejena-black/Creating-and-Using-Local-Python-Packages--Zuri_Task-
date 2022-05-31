ec8a00a1bafa17ea877aff840a0d69ad011b7b78
Nathan Onofeghara <nathanonofeghara@gmail.com>
2022-05-31 20:46:22 +0100

    code refactor

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import random
 
 # Game 
-game_options = ['R', 'P', 'S']
-game_options_moves = ['Rock', 'Paper', 'Scissors']
-game = dict(zip(game_options, game_options_moves))
+game = {'R': 'Rock', 'P': "Paper", 'S': 'Scissors'}
 
 # Computer_logic
 def computer_move():
+    game_options = ['R', 'P', 'S']
     computer_choice = random.choice(game_options)
     if computer_choice in game:
         return game[computer_choice]
@@ -19,51 +18,45 @@ def player_move():
         player_choice = player_choice.upper()
 
         if player_choice in game:
-            return game[player_choice]
-            
+            return game[player_choice]  
         else:
             print('Invalid input, Please pick from the options given \n')
 
+
+# Condition for winning
+def winner_condition():
+    list1 = ('Rock', 'Paper', 'Scissors')
+    list2 = ('Scissors', 'Rock', 'Paper')
+    winner = list(zip(list1, list2))
+    return winner
+
+
+
+# Main game logic
+def game_flow():
+    # Game loop
+    while True:
+        computer = computer_move()
+        player = player_move()
+        winner = winner_condition()
+        
+        # Player and Computer move displayed
+        print(f'\nPlayer ({player}) : CPU ({computer}) \n')
+
+        # Winner check
+        if player == computer:
+                print('It\'s a tie Play again \n')
+
+        elif (player, computer) in winner:
+            print('Player Wins')
+            break
+        elif (computer, player) in winner:
+            print('Computer Wins')
+            break
 
 
 # Game prompt
 print('Welcome to the Rock, Paper, Scissors game  \nThis game is Player vs CPU  \nYour avaliable moves are:')
 print('"R" for "Rock"  \n"P" for "Paper" \n"S" for "Scissors" ')
 
-# Main game logic
-while True:
-    com = computer_move()
-    player = player_move()
-    
-
-    # Player and Computer move displayed
-    print(f'\nPlayer ({player}) : CPU ({com}) \n')
-
-
-    # Winner check
-    if player == com:
-            print('It\'s a tie Play again \n')
-
-    elif player == 'Rock':
-        if com == 'Paper':
-            print('Computer Wins')
-            break
-        elif com == 'Scissors':
-            print('Player Wins')
-            break
-
-    elif player == 'Paper':
-        if com == 'Rock':
-            print('Player Wins')
-            break
-        elif com == 'Scissors':
-            print('Computer Wins')
-            break
-
-    elif player == 'Scissors':
-        if com == 'Rock':
-            print('Computer Wins')
-            break
-        elif com == 'Paper':
-            print('Player Wins')
-            break
+game_flow()
